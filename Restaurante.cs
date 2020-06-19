1c4d996be4fd00fec7f5b416688785f0bf4323cd
almeida0801 <eduardoalmeidasantos5@gmail.com>
2020-06-19 14:07:36 -0300

    Subindo novamente para teste

--- Restaurante.cs
+++ Restaurante.cs
@@ -2,10 +2,19 @@ namespace Aula20Ifood
 {
     public class Restaurante
     {
-       public string NomeRestaurante { get; set; }
-        public string My { get; set; }
+        public string NomeRestaurante { get; set; }
 
+        public string Endereco { get; set; }
 
+        public Restaurante(string _nome, string _endereco){
 
+            this.NomeRestaurante = _nome;
+            this.Endereco = _endereco;
+
+        }
+
+        public string MostrarDados(){
+           return $"Restaurante: {NomeRestaurante}, Endereço: {Endereco}";
+        }
     }
 }
